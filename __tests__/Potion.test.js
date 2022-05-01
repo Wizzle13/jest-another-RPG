4524ad84ae7dbf4f915809449634c10edc326d64
CBurton <burton.chris13@gmail.com>
2022-05-01 10:47:43 -0500

    Add additional test to check for random creation of potion.  Added code to create random potion

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,6 +1,16 @@
+const { equal } = require('assert');
 const Potion = require('../lib/Potion.js');
+// Tests creation of Health potion
 test('creates a health potion object', () => {
     const potion = new Potion('health');
     expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+// Test random creation of potion
+test('creates a ramdom potion object', () => {
+    const potion = new Potion();
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
 });
