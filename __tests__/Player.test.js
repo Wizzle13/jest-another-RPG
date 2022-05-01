e59e146d0e981bba4dfb62d8ccfecc6f3db822b7
CBurton <burton.chris13@gmail.com>
2022-05-01 17:22:29 -0500

    completed player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -51,3 +51,25 @@ test("Subtracts from player's health", () => {
     player.reduceHealth(99999);
     expect(player.health).toBe(0);
 });
+
+test("gets player's attack value", () => {
+    const player = new Player('Sam');
+    player.strength = 10;
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Sam');
+    const oldCount = player.inventory.length;
+    player.addPotion(new Potion());
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('uses a potion from inventory', () => {
+    const player = new Player('sSam');
+     player.inventory = [new Potion(),new Potion(),new Potion()];
+     const oldCount = player.inventory.length;
+     player.usePotion(1);
+     expect(player.inventory.length).toBeLessThan(oldCount);
+});
